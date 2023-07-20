ba4efa59ef885c698328de090118d072c3bb8715
Ansh0821 <ansh2020kumar@gmail.com>
2023-07-21 01:01:53 +0530

    final signin/up page

--- sign.js
+++ sign.js
@@ -1,13 +1,33 @@
-function toggleContent() {
-  const currentContent = document.getElementById("current");
-  const nextContent = document.getElementById("next");
+// // Function to show the Sign In page
+// function showSignInPage() {
+//   document.getElementById("current").style.display = "none";
+//   document.getElementById("signInPage").style.display = "block";
+// }
 
-  // Toggle the visibility of currentContent and nextContent
-  if (currentContent.style.display === "flex") {
-    currentContent.style.display = "none";
-    nextContent.style.display = "flex";
-  } else {
-    currentContent.style.display = "flex";
-    nextContent.style.display = "none";
-  }
-}
+// // Function to show the Sign Up page
+// function showSignUpPage() {
+//   document.getElementById("current").style.display = "none";
+//   document.getElementById("signUpPage").style.display = "block";
+// }
+
+// // Function to show the Sign Up page when "Sign Up" text is clicked
+// document.querySelector(".text a").addEventListener("click", function () {
+//   showSignUpPage();
+// });
+
+// // Function to show the Sign In page when "Sign Up" button is clicked
+// document.querySelector(".btn2").addEventListener("click", function () {
+//   showSignInPage();
+// });
+
+// // Function to show the Sign Up page
+// function showSignUpPage() {
+//   document.getElementById("current").style.display = "none";
+//   document.getElementById("signInPage").style.display = "none";
+//   document.getElementById("signUpPage").style.display = "block";
+// }
+
+// // Add event listener to "Sign Up" button in the navbar
+// document.querySelector(".btn2").addEventListener("click", function () {
+//   showSignUpPage();
+// });
